129679009542e4ab528ff38c285ebc7aedfbb566
CAIO MOUTA <caioeduardo1993@gmail.com>
2022-06-07 00:01:43 -0300

    react

--- fundamentos/src/App.jsx
+++ fundamentos/src/App.jsx
@@ -1,25 +1,45 @@
+import './App.css'
+import './components/layout/Card.css'
 import React from 'react'
 
+import Card from './components/layout/Card'
+import Aleatorio from './components/basicos/Aleatorio'
 import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
 import Fragmento from './components/basicos/Fragmento'
 
 
-export default function App(props) {
-    return (
-        <div id="app">
+export default _ =>
+    <div className ="App">
         <h1>Fundamentos React</h1>
-        <Fragmento />
-        
-        <ComParametro 
-        titulo="Banco de dados" 
-        aluno =" Mouta "
-        nota = " 9.3" />
-        <ComParametro 
-        titulo="Teoria de Grafos" 
-        aluno =" Mouta "
-        nota = " 5.9" />
-        <Primeiro></Primeiro>
-     </div>
-    );
-}
+
+        <div className='Cards'>
+            <Card titulo="Exemplo de Card">
+                <Aleatorio min={1} max={60} />
+            </Card>
+
+            
+            <Card titulo="Fragmento">
+            <Fragmento />
+            </Card>
+    
+            
+            <Card titulo="Com Parametro">
+            <ComParametro
+                titulo="Banco de dados"
+                aluno=" Mouta "
+                nota=" 9.3" />
+            <ComParametro
+                titulo="Teoria de Grafos"
+                aluno=" Mouta "
+                nota=" 5.9" />
+            </Card>
+
+            <Card titulo="Primeiro">
+            <Primeiro></Primeiro>
+            </Card>
+        </div>
+
+       
+    </div>
+
