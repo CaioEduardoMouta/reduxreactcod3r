496a49b99df768e4112b0a73b807ba94f5ae5646
CAIO MOUTA <caioeduardo1993@gmail.com>
2022-06-19 23:16:22 -0300

    react

--- hooks/src/views/examples/UseContext.jsx
+++ hooks/src/views/examples/UseContext.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 
-const UseContext = (props) => {
+import DataContext from '../../data/DataContext'
+
+const UseContext = props => {
+
+    const context = useContext(DataContext)
+
+    function setNumber(n) {
+        
+    }
+
     return (
         <div className="UseContext">
             <PageTitle
                 title="Hook UseContext"
                 subtitle="Aceita um objeto de contexto e retorna o valor atual do contexto!"
             />
+
+            <div className="center">
+                <span className="text">{context.state.text}</span>
+                <span className="text">{context.state.number}</span>
+            </div>
         </div>
     )
 }
