53f2d26a6a79479a33211b04176f74114f1d2a83
CAIO MOUTA <caioeduardo1993@gmail.com>
2022-06-26 09:44:03 -0300

    redux

--- aula_nextjs/fundamentos/src/pages/index.jsx
+++ aula_nextjs/fundamentos/src/pages/index.jsx
@@ -12,9 +12,13 @@ export default function Inicio() {
             height: '100vh'
         }}>
        
-            <Navegador texto="Estiloso" destino="/estiloso" cor="#FFAAFF"/>
-            <Navegador texto="Exemplo" destino="/exemplo" cor="#fafa"/>
-            <Navegador texto="JSX" destino="/jsx"/>
+            <Navegador texto="Estiloso" destino="/estiloso" cor="#310871"/>
+            <Navegador texto="Exemplo" destino="/exemplo" cor="#022134"/>
+            <Navegador texto="JSX" destino="/jsx" cor="#0051F0"/>
+            <Navegador texto="Navegação #01" destino="/navegacao" cor="green"/>
+            <Navegador texto="Navegação #02" destino="/cliente/sp-2/123" cor="#0090F9"/>
+            <Navegador texto="Componente com Estado" destino="/estado" cor="#003311"/>
+            <Navegador texto="Integracao com API #01" destino="/integracao_1" cor="#009911"/>
         
         </div>
     )
