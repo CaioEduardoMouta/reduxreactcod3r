ba657f25759f92f090f0b49e73061580b7a0399a
CAIO MOUTA <caioeduardo1993@gmail.com>
2022-06-28 00:06:02 -0300

    next

--- aula_nextjs/next-crud/src/pages/index.tsx
+++ aula_nextjs/next-crud/src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Botao from "../components/Botao";
+import Formulario from "../components/Formulario";
 import Layout from "../components/Layout";
 import Tabela from "../components/Tabela";
 import Cliente from "../core/Cliente";
@@ -28,10 +29,14 @@ export default function Home() {
       text-white
     `}>
       <Layout titulo="Cadastro Simples">
-        <Botao>Novo Cliente</Botao>
+        <div className="flex justify-end">
+        <Botao cor="green" className="mb-4">Novo Cliente</Botao>
+        </div>
         <Tabela clientes={clientes} 
         clienteSelecionado={clienteSelecionado}
-        clienteExcluido={clienteExcluido}/>
+        clienteExcluido={clienteExcluido}
+  />
+      <Formulario cliente={Cliente[2]}></Formulario>
       </Layout>
     </div>
       
