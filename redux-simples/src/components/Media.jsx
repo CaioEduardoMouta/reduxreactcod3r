8ac3bbef89333b183db85cdd90d4116590012b9d
CAIO MOUTA <caioeduardo1993@gmail.com>
2022-07-09 16:54:17 -0300

    Redux

---
+++ redux-simples/src/components/Media.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import Card from "./Card";
+
+export default props => {
+    const {min, max } = props
+
+    return (
+        <Card title="Média dos numeros" green>
+            <div>
+                <span>
+                    <span>Resultado:</span>
+                    <strong>{(max + min) / 2}</strong>
+                </span>
+            </div>
+        </Card>
+    )
+}
