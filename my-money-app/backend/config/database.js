2084a54427bf770c11e1a66cd01f1a3627984ed0
CAIO MOUTA <caioeduardo1993@gmail.com>
2022-07-14 00:03:31 -0300

    REDUX

--- my-money-app/backend/config/database.js
+++ my-money-app/backend/config/database.js
@@ -1,4 +1,12 @@
 const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
-module.exports = mongoose.connect('mongodb://localhost/mymoney')
+module.exports = mongoose.connect('mongodb://localhost/mymoney', { useNewUrlParser: true })
+ 
+mongoose.Error.messages.general.require = "O atributo {PATH} é obrigatorio."
+mongoose.Error.messages.Number.min =
+    "O '{VALUE}'  informado é menor que o limite mínimo de '{MIN}'."
+mongoose.Error.messages.Number.max =
+    "O '{VALUE}' informado é maior que o limiter máximo de '{MAX}'."
+mongoose.Error.messages.String.enum = 
+    "'{VALUE}' não é valor para o atributo '{PATH}'."       
