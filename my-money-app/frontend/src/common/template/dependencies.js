972df34cf67cefbe4e3269d270a8a870b57c8bf4
CAIO MOUTA <caioeduardo1993@gmail.com>
2022-07-15 06:28:46 -0300

    react

---
+++ my-money-app/frontend/src/common/template/dependencies.js
@@ -0,0 +1,8 @@
+import 'modules/admin-lte/plugins/jQueryIU/jquery-ui.min'
+import 'modules/admin-lte/plugins/fastclick/fastclick'
+import 'modules/admin-lte/plugins/slimScroll/jquery.slimscroll.min'
+import 'modules/amin-lte/dist/js/app.min'
+
+import 'modules/font-awesome/css/font-awesome.min.css'
+import 'modules/ionicons/dist/css/ionicons.min.css'
+import 'modules/'
