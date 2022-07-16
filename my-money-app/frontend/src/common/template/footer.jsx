b6cefb3a3385137a77ffd6712b55f17ac8bd64a7
CAIO MOUTA <caioeduardo1993@gmail.com>
2022-07-16 16:11:43 -0300

    react

---
+++ my-money-app/frontend/src/common/template/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footeer'>
+        <strong>
+            Copyright &copy; 2022
+            <a href='http://cod3r.com.br' 
+                target='_blank'> Cod3r</a>
+        </strong>
+    </footer>
+)
