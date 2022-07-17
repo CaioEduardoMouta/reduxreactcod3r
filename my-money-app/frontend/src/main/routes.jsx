98aed6842211430422cd5c18ed543b9fb3c32519
CAIO MOUTA <caioeduardo1993@gmail.com>
2022-07-17 09:37:27 -0300

    redux

--- my-money-app/frontend/src/main/routes.jsx
+++ my-money-app/frontend/src/main/routes.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Router, Route, Redirect, hasHistory } from 'react-router'
 import BillingCycle from '../billingCycle/billingCycle';
-import Dashboard from '../dashboard/dashboard';
+import Dashboard2 from '../dashboard2/dashboard2';
 
 export default props => (
     <Router history={hasHistory}>
-        <Route path='/' component={Dashboard} />
+        <Route path='/' component={Dashboard2} />
         <Route path='/billingCycles' component={BillingCycle} />
         <Redirect from='*' to='/' />
         
