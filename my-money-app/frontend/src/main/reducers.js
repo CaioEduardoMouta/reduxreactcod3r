b58d04ddfb20fbd150d9595f06bc123cc5470a51
CAIO MOUTA <caioeduardo1993@gmail.com>
2022-07-20 22:34:26 -0300

    react

--- my-money-app/frontend/src/main/reducers.js
+++ my-money-app/frontend/src/main/reducers.js
@@ -1,5 +1,7 @@
 
 import { combineReducers } from 'redux'
+import { reducer as formReducer } from 'redux-form'
+
 import dashboardReducer from '../dashboard/dashboardReducer'
 import TabReducer from '../common/tab/tabReducer'
 import BillingCycleReducer from '../billingCycle/billingCycleReducer'
@@ -7,7 +9,8 @@ import BillingCycleReducer from '../billingCycle/billingCycleReducer'
 const rootReducer = combineReducers({
     dashboard: dashboardReducer,
     tab: TabReducer,
-    billingCycle: BillingCycleReducer
+    billingCycle: BillingCycleReducer,
+    form: formReducer
 })
 
 export default rootReducer
