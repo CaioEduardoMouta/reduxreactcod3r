4f66660e7082461a92c598633a4c659b0df8b25b
CAIO MOUTA <caioeduardo1993@gmail.com>
2022-07-28 00:02:12 -0300

    My-Money

---
+++ my-money-app-clone/backend/src/auth/authActions.js
@@ -0,0 +1,23 @@
+import { toastr } from 'react-redux-toastr'
+import axios from 'axios'
+import consts from '../conts'
+
+function submit(values, url) {
+    return dispatch => {
+        axios.post(url,values)
+            .then(resp => {
+                dispatch([
+                    { type: 'USER_FETCHED', payload: resp.data }
+                ])
+            })
+            .catch(e => {
+                e.response.data.errors.forEach(
+                    error => toastr.error('Erro',error))
+                
+            })
+    }
+}
+
+export function logout() {
+    return{ type: 'TOKEN_VALIDATED', payload: false}
+}
