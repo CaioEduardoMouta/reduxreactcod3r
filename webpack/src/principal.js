980748bde917bb869a68ce33ef4aaa7673553039
CAIO MOUTA <caioeduardo1993@gmail.com>
2022-08-01 23:45:53 -0300

    Webpack

---
+++ webpack/src/principal.js
@@ -0,0 +1,8 @@
+//const Pessoa = require('./pessoa')
+
+import Pessoa from './pessoa'
+import './assets/css/estilo.css'
+import './modulos/moduloA'
+
+const atendente = new Pessoa
+console.log(atendente.cumprimentar())
